a1dfd655f7c148e7127f8d9178de298b9f5b6957
edwin.choi <edwin.choi@45bae1fb-b1d3-5b41-7542-4bd13db4c350>
2011-05-11 05:36:46 +0000

    updated license block

--- trunk/scbldr/_collect.php
+++ trunk/scbldr/_collect.php
@@ -1,4 +1,12 @@
 <?php
+/*!
+ * Schedule builder
+ *
+ * Copyright (c) 2011, Edwin Choi
+ *
+ * Licensed under LGPL 3.0
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 
 include "./model/objects.php";
 include "./dbconnect.php";
--- trunk/scbldr/print.php
+++ trunk/scbldr/print.php
@@ -1,4 +1,12 @@
 <?php
+/*!
+ * Schedule builder
+ *
+ * Copyright (c) 2011, Edwin Choi
+ *
+ * Licensed under LGPL 3.0
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 
 require_once "./include/timefunc.php";
 require_once "./terminfo.php";
