3e0e5a0d378b17b805866bf50a52164c44f807cd
joe.parrinello@gmail.com <jap64@afsconnect1.njit.edu>
2015-03-16 13:40:59 -0400

    git message for your changes. Lines starting

--- dbconnect.php
+++ dbconnect.php
@@ -1,13 +1,11 @@
 <?php
 /*!
- * Schedule builder
- *
- * Copyright (c) 2011, Edwin Choi
- *
- * Licensed under LGPL 3.0
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- */
-
-$conn = new mysqli("localhost", "root", "werwer", "scbldr");
-
+* Schedule builder
+*
+* Copyright (c) 2011, Edwin Choi
+*
+* Licensed under LGPL 3.0
+* http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+	$conn = new mysqli("localhost", "root", "werwer", "scbldr");
 ?>
